b8ec87f9a8a5fae0f32a2cfbc7629fdadcb83703
ada1012 <alber.diaz.toledo@gmail.com>
2021-11-17 21:32:46 +0100

    Animación botón play

--- lib/src/pages/music_player_page.dart
+++ lib/src/pages/music_player_page.dart
@@ -8,15 +8,44 @@ class MusicPlayerPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
+      body: Stack(
         children: [
-          CustomAppBar(),
-          ImagenDiscoDuracion(),
-          TituloPlay(),
-          Expanded(child: Lyrics())
-        ],
-     ),
+          Background(),
+          Column(
+            children: [
+              CustomAppBar(),
+              ImagenDiscoDuracion(),
+              TituloPlay(),
+              Expanded(child: Lyrics())
+            ],
+           ),
+        ]
+      ),
    );
+  }
+}
+
+class Background extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    final screenSize = MediaQuery.of(context).size;
+
+    return Container(
+      width: double.infinity,
+      height: screenSize.height * 0.8,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.only(bottomLeft: Radius.circular(60)),
+        gradient: LinearGradient(
+          begin: Alignment.centerLeft,
+          end: Alignment.center,
+          colors: [
+            Color(0xff33333E),
+            Color(0xff201E28)
+          ]
+        )
+      ),
+    );
   }
 }
 
@@ -38,7 +67,30 @@ class Lyrics extends StatelessWidget {
   }
 }
 
-class TituloPlay extends StatelessWidget {
+class TituloPlay extends StatefulWidget {
+
+  @override
+  _TituloPlayState createState() => _TituloPlayState();
+}
+
+class _TituloPlayState extends State<TituloPlay> with SingleTickerProviderStateMixin{
+
+  bool isPlaying = false;
+  late AnimationController playAnimation;
+
+  @override
+  void initState() {
+    playAnimation = AnimationController( vsync: this, duration: Duration(milliseconds: 500));
+
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    this.playAnimation.dispose();
+
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -58,9 +110,18 @@ class TituloPlay extends StatelessWidget {
             elevation: 0,
             highlightElevation: 0,
             backgroundColor: Color(0xffF8CB51),
-            child: Icon(Icons.play_arrow),
+            child: AnimatedIcon(
+              icon: AnimatedIcons.play_pause,
+              progress: playAnimation
+            ),
             onPressed: (){
-
+              if( this.isPlaying ){
+                playAnimation.reverse();
+                this.isPlaying = false;
+              } else {
+                playAnimation.forward();
+                this.isPlaying = true;
+              }
             },
           )
         ],
