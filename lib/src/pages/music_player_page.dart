0db8325d05d25230348e33cfbdad955689fc39f4
ada1012 <alber.diaz.toledo@gmail.com>
2021-11-17 20:28:29 +0100

    Custom appBar

--- lib/src/pages/music_player_page.dart
+++ lib/src/pages/music_player_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:music_player/src/widgets/custom_appbar.dart';
 
 
 class MusicPlayerPage extends StatelessWidget {
@@ -6,8 +7,10 @@ class MusicPlayerPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Hola Mundo'),
+      body: Column(
+        children: [
+          CustomAppBar()
+        ],
      ),
    );
   }
