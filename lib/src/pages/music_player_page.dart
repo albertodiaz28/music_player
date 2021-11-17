981eb8c60620e44eac98073a5232341645288373
ada1012 <alber.diaz.toledo@gmail.com>
2021-11-17 20:57:41 +0100

    Diseño titulo, autor y botón de play

--- lib/src/pages/music_player_page.dart
+++ lib/src/pages/music_player_page.dart
@@ -10,10 +10,42 @@ class MusicPlayerPage extends StatelessWidget {
       body: Column(
         children: [
           CustomAppBar(),
-          ImagenDiscoDuracion()
+          ImagenDiscoDuracion(),
+          TituloPlay()
         ],
      ),
    );
+  }
+}
+
+class TituloPlay extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 45),
+      margin: EdgeInsets.only(top: 30),
+      child: Row(
+        children: [
+          Column(
+            children: [
+              Text('Frío - N15', style: TextStyle( fontSize: 30, color: Colors.white.withOpacity(0.8))),
+              Text('Danie & Lasio', style: TextStyle( fontSize: 15, color: Colors.white.withOpacity(0.5))),
+            ],
+          ),
+          Spacer(),
+          FloatingActionButton(
+            elevation: 0,
+            highlightElevation: 0,
+            backgroundColor: Color(0xffF8CB51),
+            child: Icon(Icons.play_arrow),
+            onPressed: (){
+
+            },
+          )
+        ],
+      ),
+    );
   }
 }
 
